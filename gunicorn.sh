0ac211d802fe80cb8bfe999390a6ee0ce0b40b91
rubyshadows <343@holbertonschool.com>
2018-10-09 15:06:35 -0700

    updating py app to take env variable

--- gunicorn.sh
+++ gunicorn.sh
@@ -3,8 +3,8 @@ sudo apt-get install python3-pip python3-dev nginx -y
 sudo pip3 install virtualenv
 mkdir ~/myproject
 cd ~/myproject
-wget https://raw.githubusercontent.com/jamangi/AppServer/master/myproject.py
+wget -O myproject.py https://raw.githubusercontent.com/jamangi/AppServer/master/myproject.py
+
 virtualenv myprojectenv
 source myprojectenv/bin/activate
-
 pip install gunicorn flask
--- myproject.py
+++ myproject.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +7,5 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+	port = getenv('HEARTPORT', 8080)
+    app.run(host='0.0.0.0', port='')
