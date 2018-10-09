b01e28f6bc9879ff81802a685e7228e8a63d2261
rubyshadows <343@holbertonschool.com>
2018-10-09 15:31:53 -0700

    add myproject.py changes

--- myproject.py
+++ myproject.py
@@ -7,5 +7,5 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-	port = os.getenv('HEARTPORT', 5000)
+    port = os.getenv('HEARTPORT', 5000)
     app.run(host='0.0.0.0', port=port)
