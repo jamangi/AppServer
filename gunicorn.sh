613c55a75849aff6064a0676b253177a24caa61a
rubyshadows <343@holbertonschool.com>
2018-10-09 15:16:36 -0700

    removing virtualenv from script

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,10 +1,11 @@
 sudo apt-get update -y
 sudo apt-get install python3-pip python3-dev nginx -y
-sudo pip3 install virtualenv
+#sudo pip3 install virtualenv
 mkdir ~/myproject
 cd ~/myproject
-wget -O myproject.py https://raw.githubusercontent.com/jamangi/AppServer/master/myproject.py
+wget --no-cache -O myproject.py https://raw.githubusercontent.com/jamangi/AppServer/master/myproject.py
 
-virtualenv myprojectenv
-source myprojectenv/bin/activate
-pip install gunicorn flask
+#virtualenv myprojectenv
+#source myprojectenv/bin/activate
+sudo pip3 install gunicorn flask
+sudo python3 myproject.py
