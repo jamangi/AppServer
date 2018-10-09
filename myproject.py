f03604d9666d6ec25d841bd6aae65c8db5f38077
rubyshadows <343@holbertonschool.com>
2018-10-09 15:09:03 -0700

    fix python typo

--- myproject.py
+++ myproject.py
@@ -7,5 +7,5 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-	port = getenv('HEARTPORT', 8080)
+	port = os.getenv('HEARTPORT', 8080)
     app.run(host='0.0.0.0', port='')
