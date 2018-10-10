a40816d9838305db91738d42fcc3d786742a488c
rubyshadows <343@holbertonschool.com>
2018-10-09 21:09:02 -0700

    add docker setup to script

---
+++ dhsetup.sh
@@ -0,0 +1,26 @@
+sudo apt-get update -y
+sudo apt-get install python3-pip python3-dev nginx -y
+#sudo pip3 install virtualenv
+mkdir ~/myproject
+cd ~/myproject
+
+sudo git init
+sudo git remote add origin https://github.com/jamangi/AjaxTests.git
+sudo git pull origin master
+
+# wget --no-cache -O myproject.py https://raw.githubusercontent.com/jamangi/AppServer/master/myproject.py
+wget --no-cache -O docker.sh https://raw.githubusercontent.com/jamangi/AppServer/master/docker.sh && chmod u+x docker.sh && ./docker.sh
+# virtualenv myprojectenv
+# source myprojectenv/bin/activate
+sudo pip3 install gunicorn flask
+# sudo python3 myproject.py
+#gunicorn --bind 0.0.0.0:5000 myproject:app
+
+sudo wget --no-cache -O /etc/systemd/system/myproject.service https://raw.githubusercontent.com/jamangi/AppServer/master/myproject.service
+sudo systemctl start myproject
+sudo systemctl enable myproject
+
+sudo wget --no-cache -O /etc/nginx/sites-available/default https://raw.githubusercontent.com/jamangi/AppServer/master/myproject.ngx
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled
+
+sudo systemctl restart nginx
--- docker.sh
+++ docker.sh
@@ -4,12 +4,12 @@ sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
 sudo docker run hello-world
