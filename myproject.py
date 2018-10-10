b2e8d9323d908b1de0756077328d9c7bb37a1447
rubyshadows <343@holbertonschool.com>
2018-10-09 17:07:09 -0700

    add nginx and app server

--- myproject.py
+++ myproject.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
+@app.route("/test")
+def test():
+   return "<h1 style='color:blue'>Hello from Test!</h1>"
+
 if __name__ == "__main__":
     port = os.getenv('HEARTPORT', 5000)
     app.run(host='0.0.0.0', port=port)
