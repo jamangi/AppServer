56351d73b853858cf587de1dfe363f7ae3c5a6f8
Noble Mansion <42617438+jamangi@users.noreply.github.com>
2025-01-12 13:08:45 -0600

    remove redundant docker install from dhsetup.sh

--- dhsetup.sh
+++ dhsetup.sh
@@ -15,7 +15,8 @@ sudo chown -R $(whoami) *
 
 # virtualenv myprojectenv
 # source myprojectenv/bin/activate
-sudo pip3 install gunicorn flask flask_cors flask-socketio docker
+sudo pip3 install gunicorn flask flask_cors flask-socketio
+# sudo pip3 install docker
 # sudo python3 myproject.py
 #gunicorn --bind 0.0.0.0:5000 myproject:app
 
